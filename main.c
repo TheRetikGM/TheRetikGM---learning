35bba9e3fceea0a9a8ec23a6de7c74405adcabcc
TheRetikGM <theretikgm@gmail.com>
2018-11-28 22:05:10 +0000

    xddd

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <ncurses.h>
 #include <stdlib.h>
 #include "menu.h"
+#include <string.h>
 
 int main()
 {
 	int maxx,maxy;
-	int x = 20;
+	int x = 40;
 	int y = 10;
-	char items[3][1024] = {"Calculator", "Ascii art", "item3"};	
+	char items[3][1024];
+		strcpy(items[0], "Calculator");
+		strcpy(items[1], "Ascii art");
+		strcpy(items[2], "item3");
+		strcpy(items[3], "<Exit>");
 	int focused = 1;
 
 	initscr();
@@ -17,9 +22,22 @@ int main()
 
 	bkgd(COLOR_PAIR(1));
 	getmaxyx(stdscr, maxy, maxx);
-	menu(10, 40, items);
+	menu(y, x, items);
 	bkgd(COLOR_PAIR(1));
-	printw("%s", items[menu_out - 1]);
+	
+	switch(menu_out)
+	{
+		case 1:
+		{
+			strcpy(items[0], "Decimal");
+			strcpy(items[1], "Hexdecimal");
+			strcpy(items[2], "Binary");
+			strcpy(items[3], "<Back>");
+			menu(y, x, items);
+		}	
+		
+	}
+	if (strcmp(items[focused - 1], "<Exit>") == 0) return 0;
 	refresh();
 	getch();
 	endwin();
--- menu.h
+++ menu.h
@@ -1,16 +1,21 @@
+#include <ncurses.h>
+#include <string.h>
+
+
+
 int menu_out;
 
 int menu(int y, int x, char (*items) [1024])
 {
-    int maxx, maxy;
-    int focused = 1;
+	int maxx, maxy;
+	int focused = 1;
 
 	init_pair(2, COLOR_BLACK, COLOR_WHITE);
-	init_pair(3, COLOR_RED, COLOR_WHITE);
+    	init_pair(3, COLOR_RED, COLOR_WHITE);
 	init_pair(4, COLOR_WHITE, COLOR_RED);
     
-    getmaxyx(stdscr, maxy, maxx);
-    WINDOW *shadow = newwin(y, x, maxy/2 - y/2.5, maxx/2 - x/2.8);
+    	getmaxyx(stdscr, maxy, maxx);
+    	WINDOW *shadow = newwin(y, x, maxy/2 - y/2.5, maxx/2 - x/2.3);
 	WINDOW *win = newwin(y, x, maxy/2 - y/2, maxx/2 - x/2);
 	refresh();
 	wbkgd(win, COLOR_PAIR(2));
@@ -26,15 +31,20 @@ int menu(int y, int x, char (*items) [1024])
 
 	while(true)
 	{
-		for(int i = 1; i <= 3; i++)
+		for(int i = 1; i <= 4; i++)
 		{
 			if (i == focused)
 			{
 				wattron(win, COLOR_PAIR(4));
 			}
-			mvwprintw(win, i + 1, 1, items[i - 1]);
+			if ((i - 1) != 3)
+			{
+				mvwprintw(win, i + 1, 1, items[i - 1]);
+			} else {
+				mvwprintw(win, y - 2, x/2 - strlen(items[3])/2, items[i - 1]);	
+			}
 			wattroff(win, COLOR_PAIR(4));
-			wrefresh(win);	
+			wrefresh(win);
 		}
 		int choice = mvwgetch(win, y - 2, 1);
 
@@ -49,7 +59,7 @@ int menu(int y, int x, char (*items) [1024])
 			case KEY_DOWN:
 			{
 				focused++;
-				if (focused == 4) focused = 3;
+				if (focused == 5) focused = 4;
 				break;
 			}
 		}
